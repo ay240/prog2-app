ed8a9e6192362c4605b0239f74e55cb1cdd6d71d
ay240 <anyur172991@gmail.com>
2026-04-26 20:32:39 +0900

    test

--- app/src/main/java/jp/ac/gifu_u/ando/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/ando/MainActivity.java
@@ -1,24 +1,43 @@
 package jp.ac.gifu_u.ando;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultText;
+    Button fortuneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        resultText = findViewById(R.id.resultText);
+        fortuneButton = findViewById(R.id.fortuneButton);
+
+        fortuneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String[] fortunes = {
+                        "大吉 🎉",
+                        "中吉 😊",
+                        "小吉 🙂",
+                        "吉 😌",
+                        "凶 😢"
+                };
+
+                Random rand = new Random();
+                int index = rand.nextInt(fortunes.length);
+
+                resultText.setText(fortunes[index]);
+            }
         });
     }
 }
